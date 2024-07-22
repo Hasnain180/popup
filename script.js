aef6f66a7a2b5762b382486eb5ca9965b53949d8
Hasnain180 <156310886+Hasnain180@users.noreply.github.com>
2024-07-22 11:11:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+let popup = document.getElementById("popup");
+
+function openpopup(){
+    popup.classList.add("openpopup");
+}
+
+function closepopup(){
+    popup.classList.remove("openpopup");
+}
